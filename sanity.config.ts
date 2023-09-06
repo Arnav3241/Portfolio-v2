8a5080f25b50a0de7cf6f31ace7012f331f0d4ee
Arnav3241 <arnav.singh.legendary.202@gmail.com>
2023-09-07 00:07:53 +0530

    Added Author Schema!

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import project from "./sanity/schemas/AuthorSchema";
 
 const config = defineConfig({
     projectId: "omhgxsgj",
@@ -8,6 +9,7 @@ const config = defineConfig({
     apiVersion: "2023-09-06",
     basePath: "/admin",
     plugins: [deskTool()], 
+    schema: { types: [ project ] }
 })
-
+ 
 export default config;
