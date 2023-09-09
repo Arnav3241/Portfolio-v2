bfdeb0774e8200fb1ddb8e05c03c0600167ea09c
Arnav3241 <arnav.singh.legendary.202@gmail.com>
2023-09-09 20:30:19 +0530

    Fixed Responsive Design bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,20 @@ import Gist from "super-react-gist"
 export default function Home() {
   return (
     <React.Fragment>
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="flex w-full justify-center border-b h-[10vh] text-xl items-center">
           Get started editing on: &nbsp;
           <code className="font-mono font-bold"><Link href={"/admin"}>Admin Page</Link></code>
         </p>
         <div>
+          <br />
+          <br />
+          <br />
+          <br />
           <p>Just a demo.</p>
           <Gist url='https://gist.github.com/Arnav3241/ab26aa8721450decc7d3cc676a85d5e5' 
-                LoadingComponent={() => <div>Waiting for Gist...</div>} />
+                LoadingComponent={() => <div>Waiting for Gist...</div>} 
+                />
+          <br />
           <br />
         </div>
     </React.Fragment>
