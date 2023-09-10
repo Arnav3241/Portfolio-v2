2e6d29ca8539fe6dea7b8ebd3571f592bb622aef
Arnav3241 <arnav.singh.legendary.202@gmail.com>
2023-09-10 17:30:42 +0530

    Added Loding.gif

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,10 @@ export default function Home() {
           <br />
           <br />
         <div className='mx-[10vw]' >
-          <p>Just a demo.</p>
           <Gist 
                 className='gist'
                 url='https://gist.github.com/Arnav3241/ab26aa8721450decc7d3cc676a85d5e5' 
-                LoadingComponent={() => <div>Waiting for Gist...</div>} 
+                LoadingComponent={() => <div className='flex justify-center'> <Image width={`100`} height={`100`} src="/Loading.gif" alt='Loading...' /> </div> } 
                 />
           <br />
           <br />
